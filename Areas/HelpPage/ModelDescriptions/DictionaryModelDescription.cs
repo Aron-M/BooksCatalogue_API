06307c8f9022e0e84587d69d6c7f57c6e008ffcd
Aron-M <aron.moelich88@gmail.com>
2024-04-10 17:02:48 +0100

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BooksCatalogue_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/BooksCatalogueController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BooksCatalogue_API.Models;
+
+namespace BooksCatalogue_API.Controllers
+{
+    public class BooksCatalogueController : ApiController
+    {
+        [Route("api/search/{BookName}")]
+        public Book Get(string BookName)
+        {
+            using (BooksCatalogueDB_Connection_String db = new BooksCatalogueDB_Connection_String())
+            {
+
+                return db.Books.FirstOrDefault(s => s.Title == BookName);
+            }
+        }
+    }
+}
